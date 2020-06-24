229bcfa364ec0b1f242a669885651aebf49c6619
manzuo <2395722107@qq.com>
2020-06-24 18:07:16 +0800

    add payment8001 module

---
+++ cloud-provider-payment8001/src/main/java/com/manzuo/springcloud/PaymentMain8001.java
@@ -0,0 +1,20 @@
+package com.manzuo.springcloud;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA
+ *
+ * @author HDU
+ * @date 2020/6/24 17:48
+ */
+@MapperScan("com.manzuo.springcloud.dao")
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/manzuo/springcloud/controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.manzuo.springcloud.controller;
+
+import com.manzuo.springcloud.entity.CommonResult;
+import com.manzuo.springcloud.entity.Payment;
+import com.manzuo.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by IntelliJ IDEA
+ *
+ * @author HDU
+ * @date 2020/6/24 17:58
+ */
+@Slf4j
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+    @PostMapping("/create")
+    public CommonResult create(@ModelAttribute Payment payment){
+        int i = paymentService.create(payment);
+        log.info("插入的结果为:"+i);
+        if(i<0)
+            return new CommonResult(500,"插入失败");
+        else
+            return new CommonResult(200,"插入成功");
+    }
+    @GetMapping("get/{id}")
+    public CommonResult<Payment> get(@PathVariable Integer id){
+        Payment payment = paymentService.getById(id);
+        if (payment!=null){
+            return new CommonResult<Payment>(200,"查询成功",payment);
+        }
+        else {
+            return new CommonResult<Payment>(200,"查询失败");
+        }
+    }
+}
